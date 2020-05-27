bf0279172cb04c008941b2a3cb62b107ff2275f5
Adrien PARAISO <adriparaiso@free.fr>
2020-05-27 23:23:54 +0200

    Formulaire AjoutArticle avec verifications

--- ProjetAngular/src/app/ajout-article/ajout-article.component.ts
+++ ProjetAngular/src/app/ajout-article/ajout-article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ajout-article',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjoutArticleComponent implements OnInit {
 
+  // email = new FormControl('', [Validators.required, Validators.email]);
+
+  titre =  new FormControl('', [Validators.required]);
+  description =  new FormControl('', [Validators.required]);
+  corps =  new FormControl('', [Validators.required]);
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getErrorMessageTitre() {
+    if (this.titre.hasError('required')) {
+      return 'Veuillez entrer un titre.';
+    }
+
+    return '';
+  }
+
+  getErrorMessageDescription() {
+    if (this.description.hasError('required')) {
+      return 'Veuillez entrer une description.';
+    }
+
+    return '';
+  }
+
+  getErrorMessageCorps() {
+    if (this.corps.hasError('required')) {
+      return 'Veuillez remplir le corps de l\'article.';
+    }
+
+    return '';
+  }
+
+
 
 }
