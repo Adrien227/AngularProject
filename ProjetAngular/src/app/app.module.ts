e520a0b05c23c690dfc7b6b295d9633f573b7f1f
Adrien PARAISO <adriparaiso@free.fr>
2020-05-27 20:17:52 +0200

    avant implementation material

--- ProjetAngular/src/app/app.module.ts
+++ ProjetAngular/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule, // Déclare des composants pour la gestion des formulaires : https://angular.io/api/forms/FormsModule
+    FormsModule,
+    BrowserAnimationsModule, // Déclare des composants pour la gestion des formulaires : https://angular.io/api/forms/FormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
